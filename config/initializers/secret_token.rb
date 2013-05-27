170b14ad187950601b38ef44fe29fc89a1a5bbff
Sara Mooradian <sara.mooradian@gmail.com>
2013-05-26 21:11:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MEF::Application.config.secret_token = '16af8a0fd3d115caed44873e3d730bb5f7aeb4691eb2986b568842f244d04acf3f3882dbb5715b36563641ec76fc31f2bacf6f40389af2721bfbac3442d76318'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MEF::Application.config.session_store :cookie_store, key: '_MEF_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MEF::Application.config.session_store :active_record_store
