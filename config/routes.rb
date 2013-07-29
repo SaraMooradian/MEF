175e6ba9fe6b762dfa53b7a12d7ac6ba6769dd38
Sara Mooradian <sara.mooradian@gmail.com>
2013-07-28 23:04:49 -0700

    Fixed if else

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 MEF::Application.routes.draw do
+  get "users/show"
+
   resources :donors
 
 
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user
 
   root :to => 'pages#home'
   get 'about' => 'pages#about'
